2f02b912ad1f336bde7b23786ade24af0dac316f
Kody Samaroo <github email address>
2020-12-13 14:25:05 -0500

    wip 2 deliverables left

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -27,25 +27,24 @@ class Employee
         Manager.save.select {|man| man.employees == self}
     end
 
-    #def self.find_by_department(dep)
-     #   binding.pry
-      #  self.find_department.find {|ele| return self.name if ele.manager == dep}
-       #  Manager.all.find {|man| return self.name if man.department == dep }
-    #end
-
-def tax_bracket
-    Employee.all.collect {|x| x.salary.sort_by}
-        
-       if  self.salary - x.salary <= 1000
-        
-        return x.name
-        
-          binding.pry
-    
-    
-    end
-    end
+    def tax_bracket
+        arr = []
+        Employee.all.each do |emp|
+            if (self.salary - emp.salary).abs.between?(1,1000) 
+                 arr << emp.name
+            end
+        end
+        arr
     end
    
+    def self.search_by_role(pos)
+        # POS should be a role String that connects to the role in an Employee object. The following code returns an empty array tho. 
+        self.all.find_all {|emp| emp.role == pos }
+        binding.pry
+    end
+
+    def get_promoted(role)
+        # This method seems like the pets buy/sell method and the person conversation method from previous labs.
+    end
 
 end
--- run.rb
+++ run.rb
@@ -10,13 +10,13 @@ man3 = Manager.new("Darrell", "Sales", 47)
 
 # Roles
 role1 = Role.new("Salesperson")
-role2 = Role.new("Warehouse")
-role3 = Role.new("HR")
+role2 = Role.new("Recpetion")
+role3 = Role.new("Vice President")
 
 # Employees
 emp1 = Employee.new("Jim", 61000, man1, role1)
-emp2 = Employee.new("Pam", 60000, man1, role3)
-emp3 = Employee.new("Ryan", 75000, man2, role2)
+emp2 = Employee.new("Pam", 60000, man1, role2)
+emp3 = Employee.new("Ryan", 75000, man2, role3)
 
 binding.pry
 puts "done"
